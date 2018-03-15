05a6af6670a5ab3929972e43a311df2815e5a216
戴礼明 <1192297699@qq.com>
2018-03-15 19:05:39 +0800

    init

---
+++ mango-ssm/src/main/java/com/soecode/lyf/dao/StudentDao.java
@@ -0,0 +1,15 @@
+package com.soecode.lyf.dao;
+
+import com.soecode.lyf.entity.Student;
+
+/**
+ * @Author 戴礼明
+ * @Date 2018/3/15 17:12
+ * @Description
+ */
+
+public interface StudentDao {
+
+    int insertStudent(Student student);
+
+}
---
+++ mango-ssm/src/main/java/com/soecode/lyf/entity/Student.java
@@ -0,0 +1,40 @@
+package com.soecode.lyf.entity;
+
+/**
+ * @Author 戴礼明
+ * @Date 2018/3/15 17:14
+ * @Description 学生实体类
+ */
+
+public class Student {
+
+    private int id;
+
+    private String name;
+
+    private int age;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
